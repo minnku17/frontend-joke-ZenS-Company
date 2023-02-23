dad7d0fcee8ac2ba030c544ef034f1ed76315d58
Truong Thanh Hoa <truongg.thanhhoaa@gmail.com>
2023-02-23 16:02:24 +0700

    change font and regular

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,9 +4,13 @@ function Footer() {
         <>
             <div className="footer">
                 <span className="footer__text">
-                    This website is created as part of Hlsolutions program. The materials contained on this website are provided for general information only and do not constitute any form of advice. HLS assumes no responsibility for the accuracy of any particular statement and accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.
+                    This website is created as part of Hlsolutions program. The materials contained on this website are provided for general
                 </span>
-
+                <span className="footer__text">
+                    information only and do not constitute any form of advice. HLS assumes no responsibility for the accuracy of any particular statement and 
+                </span><span className="footer__text">
+                     accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.
+                </span>
                 <p>Copyright 2021 HLS</p>
             </div>
         </>
